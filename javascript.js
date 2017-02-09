358938f774f2e1d3445be522bbf1ada4602b576f
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-08 20:58:00 -0500

    Got the images on the page and got them to switch when arrows are clicked.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-$(".body").html(carouselImages);
+var i = 0;
+var currentPic= $(".pic").html(`<img src ="${carouselImages[i].image_url}"/>`)
+
+function nextPic(){
+    i = i + 1;
+    $(".pic").html(`<img src ="${carouselImages[i].image_url}"/>`)
+    $(".pic").html(`<img src ="${carouselImages[i].image_url}"/>`)
+
+    }
+
+
+function lastPic(){
+  i = i - 1;
+  $(".pic").html(`<img src ="${carouselImages[i].image_url}"/>`)
+  }
+
+
+$(".nextPic").on("click",nextPic);
+$(".lastPic").on("click",lastPic);
